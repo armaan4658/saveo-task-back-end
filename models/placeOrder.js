c8c3a3eb379b14fd971e5358be686bc4c073f7c0
armaan4658 <armaan4658@gmail.com>
2021-10-13 08:12:04 +0530

    added order schema

--- models/placeOrder.js
+++ models/placeOrder.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-    order : {
-        type : Array,
+const Orders = new mongoose.Schema({
+    c_unique_id : {
+        type : String,
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    c_name : {
+        type : String,
         required : true
     }
+})
+const orderSchema = new mongoose.Schema({
+    order : [Orders]
 })
 
 const Order = mongoose.model("order",orderSchema);
